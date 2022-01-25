d339cb82ad505b4cc892ad99ec178470dfb9f520
Isabella Chagas <bellamarchagas@gmail.com>
2022-01-25 12:07:23 -0300

    Atualizando

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1-2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1-2.js
@@ -5,11 +5,21 @@ const rectangle2 = [3, 5];
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
+
 rectangles.forEach((rectangle) => {
-  rectangleArea(...rectangle) // altere a chamada da funcao rectangleArea
-  console.log(rectangle[0] * rectangle[1]);
+ console.log(rectangleArea(...rectangle)); // altere a chamada da funcao rectangleArea
+  //console.log(rectangle[0] * rectangle[1]);
 });
 
+
+
 // 2
-const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
-console.log(sum(rectangle1));
+//const sum = (...numbers) => numbers.reduce((acc, curr) => acc + curr);
+//console.log(sum);
+const sum = (...numbers) => {
+  return numbers.reduce((acc, curr) => {
+    return acc + curr;
+  });
+};
+
+console.log(sum(1, 2, 3, 4));
