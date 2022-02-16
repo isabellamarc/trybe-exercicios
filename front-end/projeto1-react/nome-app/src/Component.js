3c930343d7883cf2f4e1a57ac133ed6cc6b3449a
Isabella Chagas <bellamarchagas@gmail.com>
2022-02-16 18:34:26 -0300

    exercicios 9.3

---
+++ front-end/projeto1-react/nome-app/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Isabella Chagas</h1>
+        <p>Sou formada em Eng Civil, tenho 30 anos. Estou em transição de carreira e adorando.</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
