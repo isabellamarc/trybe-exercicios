63d259644c60b0012a5752e17312fa056dc36fae
Isabella Chagas <bellamarchagas@gmail.com>
2022-02-17 19:12:22 -0300

    Exercicios 10.2

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+}
+
+export default App;
